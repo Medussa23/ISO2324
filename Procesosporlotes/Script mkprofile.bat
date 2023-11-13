aa16885c12bc8263cdc7f19342b23f8493c10b85
Medussa23 <150673431+Medussa23@users.noreply.github.com>
2023-11-13 12:21:29 +0100

    Add files via upload

---
+++ Procesosporlotes/Script mkprofile.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Script mkprofile que genera un árbol de directorios
+REM
+REM Auto: Alejandro González Toro
+REM Fecha:13-11-2023
+MKDIR %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\daw %USERPROFILE%\smr %USERPROFILE%\ceti
+MKDIR %USERPROFILE%\asir\docs %USERPROFILE%\asir\pract %USERPROFILE%\asir\exam
+MKDIR %USERPROFILE%\dam\docs %USERPROFILE%\dam\pract %USERPROFILE%\dam\exam
+MKDIR %USERPROFILE%\daw\docs %USERPROFILE%\daw\pract %USERPROFILE%\daw\exam
+MKDIR %USERPROFILE%\smr\docs %USERPROFILE%\smr\pract %USERPROFILE%\smr\exam
+MKDIR %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\exam
